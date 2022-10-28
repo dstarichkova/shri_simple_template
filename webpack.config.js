e5dbd11b624e633b3767129729db18b67d4f17cf
daria <dstarichkova@gmail.com>
2022-10-28 17:59:02 +0300

    commit 1 (build works, start needs fixing)

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const StatoscopePlugin = require('@statoscope/webpack-plugin').default;
+const LodashWebPackPlugin = require('lodash-webpack-plugin');
 
 const config = {
     entry: {
@@ -11,25 +12,63 @@ const config = {
         new HtmlWebpackPlugin(),
         new StatoscopePlugin({
             saveStatsTo: 'stats.json',
+            saveReportTo: 'report.html',
             saveOnlyStats: false,
             open: false,
         }),
+        new LodashWebPackPlugin(),
     ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
+        clean: true
     },
     module: {
         rules: [
-            // @TODO js rule
-            // @TODO css rule
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env',
+                        '@babel/react', {
+                            'plugins': ['@babel/plugin-proposal-class-properties']
+                        }]
+
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
         ],
     },
-    // @TODO optimizations
-    // @TODO lodash treeshaking
-    // @TODO chunk for lodash
-    // @TODO chunk for runtime
-    // @TODO fallback for crypto
+    optimization: {
+        minimize: true,
+        moduleIds: 'deterministic',
+        innerGraph: true,
+        concatenateModules: true,
+        usedExports: true,
+        splitChunks: {
+            chunks: "all"
+        },
+        runtimeChunk: {
+            name: "runtime"
+        }
+    },
+    resolve: {
+        fallback: {
+            crypto: require.resolve('crypto-browserify'),
+            stream: require.resolve("stream-browserify")
+        },
+    },
+    target: 'web',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        open: true,
+        port: 9000,
+    },
 };
 
 module.exports = config;
